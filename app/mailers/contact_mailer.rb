c031eb71c2d890e2bf6364fae5db4217be4c60bf
Faskill <fatskill@hotmail.fr>
2016-12-02 17:44:39 +0000

    Changed delivery email to my gmail adress

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'fatskill@hotmail.fr'
+    default to: 'octave.antoni@gmail.com'
     
     def contact_email(name, email, body)
       @name = name
